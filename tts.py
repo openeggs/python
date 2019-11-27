3b628ad39dbd7827e6fc1c0b96c2e92558c093e3
openeggs <45268317+openeggs@users.noreply.github.com>
2019-11-27 09:39:47 -0800

    Create tts.py

---
+++ tts.py
@@ -0,0 +1,8 @@
+from os import system
+import time
+print('''simple text to speach machine''')
+time.sleep(.5)
+print("""how it works: type what you want in the terminal and it says it out loud""")
+system('''say
+
+''')
