11fe7f322c462f22d1518a811a9065e8d38e0960
openeggs <45268317+openeggs@users.noreply.github.com>
2020-04-18 17:41:10 -0700

    Create random number art.py

---
+++ random number art.py
@@ -0,0 +1,273 @@
+import random
+import time
+def program():
+    one = random.randrange(1,3)
+    if one == 1:
+        one = ' '
+    else:
+        one = 'x'
+    two = random.randrange(1,3)
+    if two == 1:
+        two = ' '
+    else:
+        two = 'x'
+    three = random.randrange(1,3)
+    if three == 1:
+        three = ' '
+    else:
+        three = 'x'
+    four = random.randrange(1,3)
+    if four == 1:
+        four = ' '
+    else:
+        four = 'x'
+    five = random.randrange(1,3)
+    if five == 1:
+        five = ' '
+    else:
+        five = 'x'
+    six = random.randrange(1,3)
+    if six == 1:
+        six = ' '
+    else:
+        six = 'x'
+    seven = random.randrange(1,3)
+    if seven == 1:
+        seven = ' '
+    else:
+        seven = 'x'
+    eight = random.randrange(1,3)
+    if eight == 1:
+        eight = ' '
+    else:
+        eight = 'x'
+    nine = random.randrange(1,3)
+    if nine == 1:
+        nine = ' '
+    else:
+        nine = 'x'
+    ten = random.randrange(1,3)
+    if ten == 1:
+        ten = ' '
+    else:
+        ten = 'x'
+    eleven = random.randrange(1,3)
+    if eleven == 1:
+        eleven = ' '
+    else:
+        eleven = 'x'
+    twelve = random.randrange(1,3)
+    if twelve == 1:
+        twelve = ' '
+    else:
+        twelve = 'x'
+    print(one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve)
+def program2():
+    one = random.randrange(1,3)
+    if one == 1:
+        one = ' '
+    else:
+        one = 'x'
+    two = random.randrange(1,3)
+    if two == 1:
+        two = ' '
+    else:
+        two = 'x'
+    three = random.randrange(1,3)
+    if three == 1:
+        three = ' '
+    else:
+        three = 'x'
+    four = random.randrange(1,3)
+    if four == 1:
+        four = ' '
+    else:
+        four = 'x'
+    five = random.randrange(1,3)
+    if five == 1:
+        five = ' '
+    else:
+        five = 'x'
+    six = random.randrange(1,3)
+    if six == 1:
+        six = ' '
+    else:
+        six = 'x'
+    seven = random.randrange(1,3)
+    if seven == 1:
+        seven = ' '
+    else:
+        seven = 'x'
+    eight = random.randrange(1,3)
+    if eight == 1:
+        eight = ' '
+    else:
+        eight = 'x'
+    nine = random.randrange(1,3)
+    if nine == 1:
+        nine = ' '
+    else:
+        nine = 'x'
+    ten = random.randrange(1,3)
+    if ten == 1:
+        ten = ' '
+    else:
+        ten = 'x'
+    eleven = random.randrange(1,3)
+    if eleven == 1:
+        eleven = ' '
+    else:
+        eleven = 'x'
+    twelve = random.randrange(1,3)
+    if twelve == 1:
+        twelve = ' '
+    else:
+        twelve = 'x'
+    print(one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve)
+
+def program3():
+    one = random.randrange(1,3)
+    if one == 1:
+        one = ' '
+    else:
+        one = 'x'
+    two = random.randrange(1,3)
+    if two == 1:
+        two = ' '
+    else:
+        two = 'x'
+    three = random.randrange(1,3)
+    if three == 1:
+        three = ' '
+    else:
+        three = 'x'
+    four = random.randrange(1,3)
+    if four == 1:
+        four = ' '
+    else:
+        four = 'x'
+    five = random.randrange(1,3)
+    if five == 1:
+        five = ' '
+    else:
+        five = 'x'
+    six = random.randrange(1,3)
+    if six == 1:
+        six = ' '
+    else:
+        six = 'x'
+    seven = random.randrange(1,3)
+    if seven == 1:
+        seven = ' '
+    else:
+        seven = 'x'
+    eight = random.randrange(1,3)
+    if eight == 1:
+        eight = ' '
+    else:
+        eight = 'x'
+    nine = random.randrange(1,3)
+    if nine == 1:
+        nine = ' '
+    else:
+        nine = 'x'
+    ten = random.randrange(1,3)
+    if ten == 1:
+        ten = ' '
+    else:
+        ten = 'x'
+    eleven = random.randrange(1,3)
+    if eleven == 1:
+        eleven = ' '
+    else:
+        eleven = 'x'
+    twelve = random.randrange(1,3)
+    if twelve == 1:
+        twelve = ' '
+    else:
+        twelve = 'x'
+    print(one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve)
+
+def program5():
+    one = random.randrange(1,3)
+    if one == 1:
+        one = ' '
+    else:
+        one = 'x'
+    two = random.randrange(1,3)
+    if two == 1:
+        two = ' '
+    else:
+        two = 'x'
+    three = random.randrange(1,3)
+    if three == 1:
+        three = ' '
+    else:
+        three = 'x'
+    four = random.randrange(1,3)
+    if four == 1:
+        four = ' '
+    else:
+        four = 'x'
+    five = random.randrange(1,3)
+    if five == 1:
+        five = ' '
+    else:
+        five = 'x'
+    six = random.randrange(1,3)
+    if six == 1:
+        six = ' '
+    else:
+        six = 'x'
+    seven = random.randrange(1,3)
+    if seven == 1:
+        seven = ' '
+    else:
+        seven = 'x'
+    eight = random.randrange(1,3)
+    if eight == 1:
+        eight = ' '
+    else:
+        eight = 'x'
+    nine = random.randrange(1,3)
+    if nine == 1:
+        nine = ' '
+    else:
+        nine = 'x'
+    ten = random.randrange(1,3)
+    if ten == 1:
+        ten = ' '
+    else:
+        ten = 'x'
+    eleven = random.randrange(1,3)
+    if eleven == 1:
+        eleven = ' '
+    else:
+        eleven = 'x'
+    twelve = random.randrange(1,3)
+    if twelve == 1:
+        twelve = ' '
+    else:
+        twelve = 'x'
+    print(one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve)
+
+
+
+def program4():
+    program_choose = random.randrange(1,5)
+    if program_choose == 1:
+        program()
+    if program_choose == 2:
+        program2()
+    if program_choose == 3:
+        program3()
+    else:
+        program5()
+
+while True:
+    program4()
+    program4()
+    program4()
+    program4()
+    program4()
+    time.sleep(1)
